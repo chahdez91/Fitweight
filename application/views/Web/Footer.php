8591c3cf375590271ea70d4a49ae879dea9c3997
Jorge Amador <chahdez91@github.com>
2016-09-28 20:26:29 -0500

    Footer

--- application/views/Web/Footer.php
+++ application/views/Web/Footer.php
@@ -1,5 +1,31 @@
 
 		<footer>
+			<div class="row" id="divFooter">
+				<div class="col-lg-4">
+					<img src="<?php echo base_url(); ?>Resources/Images/frontpage/frontpage-29.png" />
+					<div class="div1">
+						<a>
+							<img src="<?php echo base_url(); ?>Resources/Images/frontpage/frontpage-28.png" />
+						</a>
+					</div>					
+				</div>
+				<div class="col-lg-4">
+					<img src="<?php echo base_url(); ?>Resources/Images/frontpage/frontpage-30.png" />
+					<div class="div2">
+						<a>
+							<img src="<?php echo base_url(); ?>Resources/Images/frontpage/frontpage-27.png" />
+						</a>
+					</div>					
+				</div>
+				<div class="col-lg-4">
+					<img src="<?php echo base_url(); ?>Resources/Images/frontpage/frontpage-31.png" />
+					<div class="div3">
+						<a>
+							<img src="<?php echo base_url(); ?>Resources/Images/frontpage/frontpage-26.png" />
+						</a>
+					</div>					
+				</div>
+			</div>
 		</footer>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 		<script type='text/javascript' src="<?php echo base_url(); ?>Resources/Plugins/bootstrap/js/bootstrap.min.js"></script>
