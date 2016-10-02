a8d7e487df99e7bad123fe050e01bdd8b4e7a333
Jorge Amador <chahdez91@github.com>
2016-10-01 23:19:12 -0500

    Seccion Conocenos

--- application/views/Web/Conocenos.php
+++ application/views/Web/Conocenos.php
@@ -2,17 +2,77 @@
 	<div id="divConocenos">		
 		<div class="div1">			
 			<img src="<?php echo base_url(); ?>Resources/Images/conocenos/conocenos-42.png" />
-			<img src="<?php echo base_url(); ?>Resources/Images/conocenos/conocenos-43" />
+			<img src="<?php echo base_url(); ?>Resources/Images/conocenos/conocenos-43.png" />
 		</div>
 		<div class="row">
-			<div class="col-lg-4">
-				<img src="<?php echo base_url(); ?>Resources/Images/conocenos/conocenos-34" />
+			<div class="col-lg-4">				
+				<div class="div100px">
+					<img src="<?php echo base_url(); ?>Resources/Images/conocenos/conocenos-34.png" />
+				</div>								
+				<div class="backgroundAzul">
+					<p>
+						Nuestra MISIÓN es lograr la plena satisfacción de nuestros clientes mediante la innovación, constante de nuestros productos y servicios de calidad y eficacia, a un bajo costo mediante la mejora continua.
+					</p>
+				</div>
 			</div>
-			<div class="col-lg-4">
-				<img src="<?php echo base_url(); ?>Resources/Images/conocenos/conocenos-34" />
+			<div class="col-lg-4">				
+				<div class="div100px">
+					<img src="<?php echo base_url(); ?>Resources/Images/conocenos/conocenos-35.png" />
+				</div>	
+				<div class="backgroundVerde">
+					<p>
+						Tenemos la VISIÓN de ser la mejor opción en la distribución y comercialización de productos para el cuidado de la salud, así mismo consolidarnos como la mejor propuesta de negocios para los profesionales expertos en bariatria, preservando nuestra integridad, transparencia y con la mejor calidad en todos los servicios y productos que distribuimos.
+					</p>
+				</div>							
 			</div>
-			<div class="col-lg-4">
-				<img src="<?php echo base_url(); ?>Resources/Images/conocenos/conocenos-34" />
+			<div class="col-lg-4">				
+				<div class="div100px">
+					<img src="<?php echo base_url(); ?>Resources/Images/conocenos/conocenos-36.png" />
+				</div>	
+				<div class="backgroundRojo">
+					<p>
+						Estamos convencidos y nos guiamos por los VALORES de la empresa que son:
+					</p>
+					<div class="divListas">
+						<ul>						
+							<li>
+								Calidad Humana 
+							</li>
+							<li>
+								Honestidad Ética 
+							</li>
+							<li>
+								Profesionalismo 
+							</li>
+							<li>
+								Respeto 
+							</li>
+							<li>
+								Seguridad
+							</li>
+							<li>
+								Pasión por lo que hacemos 
+							</li>
+						</ul>
+						<ul>						
+							<li>
+								Positivismo
+							</li>
+							<li>
+								Sinergia
+							</li>
+							<li>
+								Creatividad
+							</li>
+							<li>
+								Lealtad
+							</li>
+							<li>
+								Confianza
+							</li>								
+						</ul>
+					</div>
+				</div>							
 			</div>
 		</div>
 	</div>
